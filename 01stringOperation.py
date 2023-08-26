d9dafccfff41fda358b6288912732e84a0ae266a
Sundar <sunndarper@gmail.com>
2023-08-26 11:25:16 -0400

    commited
    Signed-off-by: Sundar <sunndarper@gmail.com>

---
+++ 01stringOperation.py
@@ -0,0 +1,24 @@
+
+print("Hello Data Structure")
+
+varString = " this is my first line which is split in multiple line"
+print(varString)
+
+varString = " this is my first line which is \n split in multiple line"
+print(varString)
+
+varString = " this is my first line which is \\n split in multiple line"
+print(varString)
+
+varString2 = ", this is my second line"
+print(varString2)
+print(varString,varString2)
+print(varString+varString2)
+varString3 = 3
+print(varString+varString2+str(varString3))
+print(varString,varString2,str(varString3))
+
+print(type(varString))
+print((type(varString2)))
+print(type(varString3))
+
