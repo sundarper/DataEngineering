9a541584227e954a5020df3bf32771bb43afed2e
Sundar <sunndarper@gmail.com>
2023-08-26 12:05:14 -0400

    commited
    Signed-off-by: Sundar <sunndarper@gmail.com>

---
+++ 02dataType.py
@@ -0,0 +1,21 @@
+
+print("This is Python data type example")
+
+varInt1 = 5
+varInt2 = 10
+varFlt1 = 2.7
+varFlt2 = 4.9
+varNone = None
+varInt3 = varInt1+varInt2+varFlt1+int(varFlt2)
+print(varInt3)
+print(type(varInt1), type(varFlt1))
+print(type(varNone))
+
+varComp=10+10j
+print(type(varComp))
+
+varList=["Sundar", 200, '10', '30', 'Rajan', "Python"]
+print(varList)
+print(type(varList))
+
+
